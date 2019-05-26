5c0c16037b06872fb664b7fb0ebebb33423223c4
Omri Fridental <4drive13@gmail.com>
2019-05-26 13:44:38 +0300

    First Version

--- Ex3/App_Start/RouteConfig.cs
+++ Ex3/App_Start/RouteConfig.cs
@@ -12,12 +12,19 @@ namespace Ex3
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.MapRoute(
+				name: "Default",
+				url: "",
+				defaults: new
+				{
+					controller = "Home",
+					action = "Index",
+					id = UrlParameter.Optional
+				}
+			);
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
+			routes.MapRoute("display", "display/{ip}/{port}",
+				defaults: new { controller = "Display", action = "Display" });
+		}
     }
 }
